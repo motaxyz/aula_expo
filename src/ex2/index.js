0aae40eaea9ad579620dbfea940e237ebe45c55c
Matheus Mota Tonini <138072866+motaxyz@users.noreply.github.com>
2024-02-28 22:42:33 -0300

    Aula 28/02

---
+++ src/ex2/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function Mensagem() {
+    return (
+        <View>
+            <Text>
+                Hello React-Native
+            </Text>
+        </View>
+    );
+}
