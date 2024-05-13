66c6ca069267221a2e2f0546557718f390e7a233
Matheus Mota Tonini <138072866+motaxyz@users.noreply.github.com>
2024-05-13 20:40:27 -0300

    Botão calcular
    criando e estilizando o botão calcular

---
+++ src/ex6/botao/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    botao: {
+        width: '100%',
+        backgroundColor: 'grey',
+        // backgroundColor: '#6dc4a4',
+        margin: 10,
+        borderRadius: 20,
+    },
+    txtBotao: {
+        fontSize: 25,
+        color: '#6dc4a4',
+        textAlign: 'center',
+        padding: 20,
+        fontWeight: 'bold',
+    },
+    btnPress: {
+        borderWidth: 4,
+        
+        borderColor: '#6dc4a4'
+    }
+    
+})
+
+export default styles;
